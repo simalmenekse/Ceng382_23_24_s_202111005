509159cfec26119010906f806554ac808dc10556
Ayşe Şimal Menekşe <94192946+simalmenekse@users.noreply.github.com>
2024-05-09 16:52:23 +0300

    Opened a new Branch for Lab 11

--- RazorPages/Models/Room.cs
+++
@@ -1,9 +0,0 @@
-namespace RazorPages.Models{
-    public class Room
-    {
-        public int Id { get; set; }
-        public string? RoomName { get; set; }
-        public int Capacity { get; set; }
-    }
-    }
-
--- RazorPages/wwwroot/js/rooms.js
+++
@@ -1,8 +0,0 @@
-    function validateCapacity() {
-        var capacity = document.getElementById("capacity").value;
-        if (capacity <= 0) {
-            document.getElementById("capacityError").style.display = "block";
-            return false; 
-        }
-        return true; 
-    }
